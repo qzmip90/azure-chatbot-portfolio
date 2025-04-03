ee70c76cabad76f290794af1c2cb10eb70ade4d4
qzmip90 <qz.mip.90@gmail.com>
2025-04-03 13:43:39 +0200

    Fix: use correct port for Azure deployment

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,4 @@ def static_files(path):
 
 if __name__ == "__main__":
     print("🚀 Flask app is starting...")
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=80)
